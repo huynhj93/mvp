3a3461b0300067c42c5f58e8de21c18e3a08990e
John Huynh <huynh.j93@gmail.com>
2016-10-11 16:14:25 -0700

    Able to retrieve date and give stats

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -8,8 +8,21 @@ angular.module('app.home',[])
 		vm.playersID =[];
 		vm.playerID = [];
 		vm.playerStats=[];
+		vm.gameID ="";
+		vm.everyGameIDPlayerPlayed =[];
+		vm.games = [];
 		vm.moreThanOnePlayer = false;
 		vm.hasData = false;
+		vm.finalStats =[];
+		vm.showStats = false;
+
+		vm.points = 0;
+		vm.assists = 0;
+		vm.blocks = 0;
+		vm.rebounds = 0;
+		vm.steals = 0;
+		vm.opponentID;
+		vm.opponent;
 		vm.teamClick = function(){
 			// homeFactory.getRoute()
 			// 	.then(function(resp){
@@ -17,13 +30,14 @@ angular.module('app.home',[])
 			// 	})
 			homeFactory.getTeams(vm.teamInput)
 				.then(function(resp){
-					console.log('resp is:',resp);
+					console.log('team id response is:',resp.data);
 					vm.teams = (resp.data);
 				})
 				.catch(function(err){
 					console.log('err is:',err);
 				})
 		};
+		vm.teamClick();
 		vm.initialized = function(){
 			homeFactory.getPlayerID(vm.playerInput)
 				.then(function(resp){
@@ -52,9 +66,46 @@ angular.module('app.home',[])
 						console.log("should have player id:",id);
 						homeFactory.getPlayerStats(id)
 							.then(function(resp){
-								console.log('the response to playerstats is:',resp);
+								console.log('the response to playerstats is:',resp.data);
 								vm.playerStats = resp.data;
 								vm.hasData = true;
+								var arr1 = [];
+								for (var i=0; i< vm.playerStats.length; i++){
+
+									var player = vm.playerStats[i];
+									arr1.push(player.game_id);
+								}
+								vm.everyGameIDPlayerPlayed = arr1;	
+
+								console.log("game id array is",vm.everyGameIDPlayerPlayed);
+								// var game = vm.everyGameIDPlayerPlayed[33];
+								// homeFactory.getGames(game)
+								// 	.then(function(resp){
+								// 		console.log("player game stats from game 0 are:",resp);
+								// 	})
+								var games = [];
+								vm.games = [];
+
+								for (var i=0; i <vm.everyGameIDPlayerPlayed.length;i++){
+									// if (i === vm.everyGameIDPlayerPlayed.length-1){
+									// 	console.log("vm.games should contain every game",vm.games);
+
+									// }
+
+									var gameI= vm.everyGameIDPlayerPlayed[i];
+									console.log(gameI);
+									homeFactory.getGames(gameI)
+										.then(function(resp){
+											// console.log("player game stats inside the for loop is:",resp);
+											// games.push(resp.data[0]);
+											resp.data[0].date = resp.data[0].date.slice(0,10);
+											vm.games.push(resp.data[0]);
+											console.log('vm games should contatin 1 game only',vm.games);
+										})
+								}
+								// vm.games = games;
+								//for each game, get the date of the game
+
 							})
 					}
 					// vm.playersID.push(resp.data[0].player_id);
@@ -62,6 +113,43 @@ angular.module('app.home',[])
 				})
 
 		};
+		vm.dateClick = function(){
+			vm.finalStats = [];
+			console.log("I WAS CLICKEDDDD");
+			var id =0;
+			for (var i=0; i <vm.games.length;i++){
+				var game = vm.games[i];
+				var date = game.date.slice(0,10);
+				console.log("the date is:",date);
+				if (date === vm.gameDateInput){
+					id = game.id;
+					break;
+				}
+			}
+			for (var j=0;j<vm.playerStats.length;j++){
+				var gameStats = vm.playerStats[j];
+				if (gameStats.game_id === id){
+					vm.points = gameStats.box_pts;
+					vm.assists = gameStats.box_ast;
+					vm.blocks = gameStats.box_blk;
+					vm.rebounds = Number(gameStats.box_oreb)+Number(gameStats.box_dreb);
+					vm.steals = gameStats.box_stl;
+					vm.opponentID = gameStats.opponent_id;
+					console.log("I MADE IT!");
+				}
+			}
+			for (var k=0; k < vm.teams.length; k++){
+				if (vm.teams[k].team_id === vm.opponentID){
+					vm.opponent = vm.teams[k].team_name;
+					vm.showStats = true;
+
+				}
+			}
+			// for (var i=0; i<vm.playerStats.length;i++){
+			// 	var obj = vm.playerStats[i];
+			// 	if (obj.game_id ===){}
+			// }
+		}
 		vm.initialized();
 	})
 	.factory('homeFactory',function($http){
@@ -73,6 +161,16 @@ angular.module('app.home',[])
 			})
 
 		};
+		var getGames = function(gameID){
+			return $http({
+				method: 'POST',
+				url:'http://api.probasketballapi.com/game',
+				params:{
+					api_key: window.API_KEY,
+					game_id: gameID
+				}
+			})
+		}
 		var getTeams = function(params){
 			return $http({
 				method: 'POST',
@@ -126,6 +224,7 @@ angular.module('app.home',[])
 
 		}
 		return {
+			getGames: getGames,
 			getPlayerStats: getPlayerStats,
 			getPlayerID: getPlayerID,
 			getRoute:getRoute,
